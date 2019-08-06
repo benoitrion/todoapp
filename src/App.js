47ca9e74074c51f6c2f6e3a20a8479ea5d7aae02
Benoit Rion <benoitrion@gmail.com>
2019-08-06 14:40:31 +0200

    start implementing app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './components/Header.js';
+import './components/TaskList.js';
+import './components/Footer.js';
+import { ALL } from '../constants';
 
 function App() {
+  const taskList = [
+    { id: 't1', title: 'Board the plane', completed: false, editing: false },
+    { id: 't2', title: 'Sleep', completed: false, editing: false },
+    {
+      id: 't3',
+      title: 'Try to finish conference slides',
+      completed: false,
+      editing: false
+    },
+    {
+      id: 't4',
+      title: 'Eat cheese and drink wine',
+      completed: false,
+      editing: false
+    },
+    { id: 't5', title: 'Go around in Uber', completed: false, editing: false },
+    {
+      id: 't6',
+      title: 'Talk with conf attendees',
+      completed: false,
+      editing: false
+    },
+    { id: 't7', title: 'Show Demo 1', completed: false, editing: false },
+    { id: 't8', title: 'Show Demo 2', completed: false, editing: false },
+    {
+      id: 't9',
+      title: 'Lament about the state of animation',
+      completed: false,
+      editing: false
+    },
+    { id: 't10', title: 'Show Secret Demo', completed: false, editing: false },
+    { id: 't11', title: 'Go home', completed: false, editing: false }
+  ];
+  const [tasks, setTasks] = useState(taskList);
+  const [value, setValue] = useState('');
+  const [selected, setSelected] = useState('all');
+
+  function onClearCompleted() {}
+
+  const activeTodoCount = tasks.filter(t => !t.completed).length;
+  const completedCount = tasks.lenght - activeTodoCount.length;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="todoapp">
+      <Header value={value} />
+      <TaskList tasks={tasks} selected={selected} />
+      <Footer
+        counter={activeTodoCount}
+        showing={ALL}
+        completedCount={completedCount}
+        onClearCompleted={this.onClearCompleted}
+      />
+    </section>
   );
 }
 
